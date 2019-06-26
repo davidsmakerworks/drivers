876d9786eb83ce4b8b88839ac76d303e211a77c2
David Rice <11968726+davidsmakerworks@users.noreply.github.com>
2019-06-25 19:03:02 -0700

    Add MIT license, add more flexible pin control macro definitions

--- nrf24L01P/nRF24L01P.c
+++ nrf24L01P/nRF24L01P.c
@@ -1,16 +1,36 @@
 /*
  * Driver for Nordic Semiconductor nRF24L01+
- * 
- * Designed and implemented by David Rice
+ * Copyright (c) 2019 David Rice
  * 
  * Requires definitions for:
- * NRF24_CSN - CSN (active low) pin on nRF24L01+
- * NRF24_CE - Chip Enable pin on nRF24L01+
+ * NRF24_CSN - CSN (active low) pin on nRF24L01+ (LEGACY)
+ * NRF24_CE - Chip Enable pin on nRF24L01+ (LEGACY)
+ * NRF24_CSN_ACTIVE() - Macro to set CSN pin low
+ * NRF24_CSN_IDLE() - Macro to set CSN pin high
+ * NRF24_CE_ACTIVE() - Macro to set CE pin high
+ * NRF24_CE_IDLE() - Macro to set CE pin low
  * NRF24_IRQ - Interrupt pin (active low) on nRF24L01+
  * NRF24_XFER_SPI(x) - Transfer one byte to/from SPI bus without changing CSN
  * 
  * Configuration is usually located in nRF24L01P-cfg.h in the same folder with the main project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */ 
 
 #include <stdint.h>
@@ -20,12 +40,12 @@
 /* Write the specified value to a single-byte register */
 void nrf24_write_register(uint8_t reg, uint8_t value)
 {
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_W_REGISTER | reg);
     NRF24_XFER_SPI(value);
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
 }
 
 /* Write values from a buffer to a multi-byte register */
@@ -33,7 +53,7 @@ void nrf24_write_register_multi(uint8_t reg, uint8_t *buf, uint8_t len)
 {
     uint8_t i;
     
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_W_REGISTER | reg);
     
@@ -41,7 +61,7 @@ void nrf24_write_register_multi(uint8_t reg, uint8_t *buf, uint8_t len)
         NRF24_XFER_SPI(buf[i]);
     }
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
 }
 
 /* Read the value of a single-byte register */
@@ -49,12 +69,12 @@ uint8_t nrf24_read_register(uint8_t reg)
 {
     uint8_t value; 
     
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_R_REGISTER | reg);
     value = NRF24_XFER_SPI(NRF24_SPI_NOP);
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
     
     return value;
 }
@@ -64,7 +84,7 @@ void nrf24_read_register_multi(uint8_t reg, uint8_t *buf, uint8_t len)
 {
     uint8_t i;
     
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_R_REGISTER | reg);
     
@@ -72,7 +92,7 @@ void nrf24_read_register_multi(uint8_t reg, uint8_t *buf, uint8_t len)
         buf[i] = NRF24_XFER_SPI(NRF24_SPI_NOP);
     }
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
 }
 
 /* Set the specified bits in a single-byte register - performs read/modify/write */
@@ -102,21 +122,21 @@ void nrf24_clear_register_bits(uint8_t reg, uint8_t bits)
 /* Flush transmit FIFO */
 void nrf24_flush_tx(void)
 {
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_FLUSH_TX);
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
 }
 
 /* Flush receive FIFO */
 void nrf24_flush_rx(void)
 {
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_FLUSH_RX);
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
 }
 
 /* Set transmit address */
@@ -136,7 +156,7 @@ void nrf24_write_payload(uint8_t *buffer, uint8_t len)
 {
     uint8_t i;
     
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_W_TX_PAYLOAD);
     
@@ -144,7 +164,7 @@ void nrf24_write_payload(uint8_t *buffer, uint8_t len)
         NRF24_XFER_SPI(buffer[i]);
     }
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
 }
 
 /* Read data from receive FIFO */
@@ -152,7 +172,7 @@ void nrf24_read_payload(uint8_t *buffer, uint8_t len)
 {
     uint8_t i;
     
-    NRF24_CSN = 0;
+    NRF24_CSN_ACTIVE();
     
     NRF24_XFER_SPI(NRF24_R_RX_PAYLOAD);
     
@@ -160,5 +180,5 @@ void nrf24_read_payload(uint8_t *buffer, uint8_t len)
         buffer[i] = NRF24_XFER_SPI(NRF24_SPI_NOP);
     }
     
-    NRF24_CSN = 1;
+    NRF24_CSN_IDLE();
 }
